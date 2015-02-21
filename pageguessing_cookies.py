eaee6d8f50b3702b50adcb67870da00dea3b8d8a
Ye <yxj9708@rit.edu>
2015-02-21 18:13:24 -0500

    Created page guessing function and cookies function in pageguessing_cookies.py. In dosc folder, extensionList.txt lists different type of extension, mywords.txt lists names of pages, URLList.txt lists the all of the linked page from Link discovery.

---
+++ pageguessing_cookies.py
@@ -0,0 +1,48 @@
+import sys
+import requests
+
+def main():
+
+	#execute page guessing function
+	pageGuassingList = pageGuessing()
+	print("The list of the unlinked pages:")
+	for page in pageGuassingList:
+		print(page)
+	
+	#execute discovering cookie function
+	cookies = readCookies()
+	print("The cookies of the website :")
+	print(cookies)
+		
+def pageGuessing():
+
+	#Read the url list.
+	with open('docs/URLList.txt') as f:
+		URLList = f.read().splitlines()
+
+	#Read the url extensions
+	with open('docs/extensionList.txt') as f:
+		URLExtensionList = f.read().splitlines()
+	
+	#Read the word list of 
+	wordsListFileName = sys.argv[3][15:]
+	with open('docs/'+ wordsListFileName) as f:
+		wordList = f.read().splitlines()
+	pages = []
+	
+	for URL in URLList:
+		for word in wordList:
+			for extension in URLExtensionList:
+				newURL = URL+word+extension
+				r = requests.get(newURL)
+				if r.status_code == 200:
+					pages.append(newURL)
+	return pages
+	
+def readCookies():
+	r = requests.Session()
+	r.get(sys.argv[2])
+	return r.cookies
+	
+if __name__ == "__main__":
+	main()
