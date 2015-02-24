c6b79fb58e24d5ac94bee67a9eba69d05d807dcb
cburtner007 <cburtner007@gmail.com>
2015-02-24 00:27:49 -0500

    FormParams

--- zfuzzer.py
+++ zfuzzer.py
@@ -155,8 +155,29 @@ def parseURL(url, dict) :
 
     return dict
 
-def formParameters(s,url):
-	return
+def formParameters(url, dict):
+
+    urlSplit = re.split("[=?&]",url)  #Split on URL params
+    baseUrl = urlSplit[0]   #This is the base URL
+
+    r = requests.get(url)
+    inputElements = re.findall("<input.*?/>",r.content) #Find all "input" elements using a non-greedy regex
+
+    for x in range(0, len(inputElements)):
+        if "name=\"" in inputElements[x]: #Check to see if the result contains a "name" attribute
+            inputName = re.findall("name=\"(.*?)\"", inputElements[x]) #Get a list that only contains the value of the "name" attribute
+            print inputName
+
+            if baseUrl in dict :  #If this URL is already in the dictionary...
+                checkList = dict[baseUrl] #Put any already created params in a temp list
+                if inputName not in checkList : #Check if item we're trying to add is in that temp list
+                    dict[baseUrl].append(result[x])
+            else :
+                 dict[baseUrl] = inputName
+
+
+
+    return dict
 
 def cookies(s):
 	return s.cookies
