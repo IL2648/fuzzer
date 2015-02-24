54cca2fbd62babaa7dd0d31ba2bc48aaa337a147
cburtner007 <cburtner007@gmail.com>
2015-02-24 00:08:49 -0500

    Copy pasted URL parse

--- zfuzzer.py
+++ zfuzzer.py
@@ -134,8 +134,23 @@ def pageGuessing(s,url):
 def inputDiscovery(s,url):
 	return
 
-def parseURLs(s,url):
-	return
+def parseURL(url, dict) :
+    print "parsing..."
+    print
+    result = re.split("[=?&]",url)  #Split on URL params
+
+    for x in range(1, len(result)) :  #Iterate through each split item
+        if(x % 2 ==1) :     #If the item is odd, then it is a param
+
+            if result[0] in dict :  #If this URL is already in the dictionary...
+                checkList = dict[result[0]] #Put any already created params in a temp list
+                if result[x] not in checkList : #Check if item we're trying to add is in that temp list
+                    dict[result[0]].append(result[x])
+            else :
+                dict[result[0]] = [result[x]]
+        x+=1
+
+    return dict
 
 def formParameters(s,url):
 	return
