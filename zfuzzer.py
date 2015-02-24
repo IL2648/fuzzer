3ef180e09b1e71c4a2e88490bfdf0bd71dda73e3
cburtner007 <cburtner007@gmail.com>
2015-02-24 07:13:42 -0500

    Last mistake fixed?

--- zfuzzer.py
+++ zfuzzer.py
@@ -177,14 +177,13 @@ def formParameters(s, url, dict):
     urlSplit = re.split("[=?&]",url)  #Split on URL params
     baseUrl = urlSplit[0]   #This is the base URL
 
-    r = s.get(url)
+    r = requests.get(url)
     inputElements = re.findall("<input.*?/>",r.content) #Find all "input" elements using a non-greedy regex
 
     for x in range(0, len(inputElements)):
         if "name=\"" in inputElements[x]: #Check to see if the result contains a "name" attribute
             inputName = re.findall("name=\"(.*?)\"", inputElements[x]) #Get a list that only contains the value of the "name" attribute
-            print inputName
-
+            inputName = inputName.pop()
             if baseUrl in dict :  #If this URL is already in the dictionary...
                 checkList = dict[baseUrl] #Put any already created params in a temp list
                 if inputName not in checkList : #Check if item we're trying to add is in that temp list
