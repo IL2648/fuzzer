67c9254c504cda364c93fc6e0680b681d0686619
Zachary Nielsen <zen.gameman@gmail.com>
2015-03-16 16:21:32 -0400

    final commit of sanitization
    if you run sanitization.py you can see how it works, if there is ANY
    confusion during integration do not hesitate to message me. Basically if
    you call sanitization, and the three print functions then it works as
    intended

--- fuzz.py
+++ fuzz.py
@@ -28,7 +28,10 @@ urlInputs = []
 urlInputDict = {}
 formInputDict = {}
 words = []
+unsanitized = []
 statusCodeLog = []
+badChars = ["<",">","\"","\\"]
+slowLog = []
 BaseUrl = ''
 TO_FILE = True
 logFile = open('output.txt', 'w')
@@ -348,4 +351,74 @@ def checkResponse(r):
     if( r.status_code >= 300 ):
         statusCodeLog.append((r.status_code,newURL))
 
+currentMilliTime = lambda: int(round(time.time() * 1000))
+def sanitization(s):
+    checkForms(s)
+    checkURLs(s)
+
+def checkForms(s):
+    for k, v in formInputDict.items():
+        url = k + "?"
+        for e in v:
+            url = url + e + "=<>\\\"&"
+        start = currentMilliTime()
+        r = s.post(url)
+        finish = currentMilliTime()
+        if(finish - start > slowTime):
+            slowLog.append((url,finish-start))
+        isSanitized(r)
+        checkResponse(r)
+
+def checkURLs(s):
+    for k, v in urlInputDict.items():
+        url = k + "?"
+        for e in v:
+            url = url + e + "=<>\\\"&"
+        start = currentMilliTime()
+        r = s.get(url)
+        finish = currentMilliTime()
+        if(finish - start > args['slow']):
+            slowLog.append((url,finish-start))
+        isSanitized(r)
+        checkResponse(r)
+
+def isSanitized(r):
+    for e in badChars:
+        if e in r.url:
+            unsanitized.append(r.url)
+            break
+
+def checkResponse(r):
+    if( r.status_code >= 300 ):
+        statusCodeLog.append((r.status_code,r.url))
+
+def printSanitizationResults():
+    print "-----------------------------------------------"
+    print "----------------UNSANITIZED URLs---------------"
+    print "-----------------------------------------------"
+    for e in unsanitized:
+        print e
+        print "-----------------------------------------------"
+    print "------------END OF UNSANITIZED URLs------------"
+    print "-----------------------------------------------"
+
+def printResponseErrors():
+    print "-----------------------------------------------"
+    print "-----------------HTTP ERRORS-------------------"
+    print "-----------------------------------------------"
+    for c,u in statusCodeLog:
+        print "("+str(c)+" : "+u+")"
+        print "-----------------------------------------------"
+    print "-------------END OF HTTP ERRORS----------------"
+    print "-----------------------------------------------"
+
+def printSlowResponses():
+    print "-----------------------------------------------"
+    print "-----------------SLOW RESPONSES----------------"
+    print "-----------------------------------------------"
+    for u,t in slowLog:
+        print "("+str(t)+" : "+u+")"
+        print "-----------------------------------------------"
+    print "---------------END OF SLOW RESPONSES-----------"
+    print "-----------------------------------------------"
 main()
--- sanitization.py
+++ sanitization.py
@@ -6,49 +6,83 @@ unsanitized = []
 statusCodeLog = []
 badChars = ["<",">","\"","\\"]
 slowLog = []
-slowTime = 500
+slowTime = 50
 currentMilliTime = lambda: int(round(time.time() * 1000))
 def sanitization(s):
-	checkForms(s)
-	checkURLs(s)
+    checkForms(s)
+    checkURLs(s)
 
 def checkForms(s):
-	for k, v in formInputDict.items():
-		url = k + "?"
-		for e in v:
-			url = url + e + "=<>\\\"&"
-		start = currentMilliTime()
-		r = s.post(url)
-		finish = currentMilliTime()
-		if(finish - start > slowTime):
-			slowLog.append(url)
-		isSanitized(r)
-		checkResponse(r, e)
+    for k, v in formInputDict.items():
+        url = k + "?"
+        for e in v:
+            url = url + e + "=<>\\\"&"
+        start = currentMilliTime()
+        r = s.post(url)
+        finish = currentMilliTime()
+        if(finish - start > slowTime):
+            slowLog.append((url,finish-start))
+        isSanitized(r)
+        checkResponse(r)
 
 def checkURLs(s):
-	for k, v in urlInputDict.items():
-		url = k + "?"
-		for e in v:
-			url = url + e + "=<>\\\"&"
-		start = currentMilliTime()
-		r = s.post(url)
-		finish = currentMilliTime()
-		if(finish - start > slowTime):
-			slowLog.append(url)
-		isSanitized(r)
-		checkResponse(r, e)
+    for k, v in urlInputDict.items():
+        url = k + "?"
+        for e in v:
+            url = url + e + "=<>\\\"&"
+        start = currentMilliTime()
+        r = s.get(url)
+        finish = currentMilliTime()
+        if(finish - start > slowTime):
+            slowLog.append((url,finish-start))
+        isSanitized(r)
+        checkResponse(r)
 
 def isSanitized(r):
-	for e in badChars:
-		if e in r.url:
-			print "unsanitary"
-			unsanitized.append(r.url)
-			break
-	print "sanitary"
+    for e in badChars:
+        if e in r.url:
+            unsanitized.append(r.url)
+            break
 
-def checkResponse(r, e):
+def checkResponse(r):
     if( r.status_code >= 300 ):
-        statusCodeLog.append((r.status_code,e))
+        statusCodeLog.append((r.status_code,r.url))
+
+def printSanitizationResults():
+    print "-----------------------------------------------"
+    print "----------------UNSANITIZED URLs---------------"
+    print "-----------------------------------------------"
+    for e in unsanitized:
+        print e
+        print "-----------------------------------------------"
+    print "------------END OF UNSANITIZED URLs------------"
+    print "-----------------------------------------------"
+
+def printResponseErrors():
+    print "-----------------------------------------------"
+    print "-----------------HTTP ERRORS-------------------"
+    print "-----------------------------------------------"
+    for c,u in statusCodeLog:
+        print "("+str(c)+" : "+u+")"
+        print "-----------------------------------------------"
+    print "-------------END OF HTTP ERRORS----------------"
+    print "-----------------------------------------------"
+
+def printSlowResponses():
+    print "-----------------------------------------------"
+    print "-----------------SLOW RESPONSES----------------"
+    print "-----------------------------------------------"
+    for u,t in slowLog:
+        print "("+str(t)+" : "+u+")"
+        print "-----------------------------------------------"
+    print "---------------END OF SLOW RESPONSES-----------"
+    print "-----------------------------------------------"
 
 with requests.Session() as s:
 	sanitization(s)
+	printSanitizationResults()
+	print "\r\n\r\n\r\n"
+	printResponseErrors()
+	print "\r\n\r\n\r\n"
+	printSlowResponses()
+	print "\r\n\r\n\r\n"
