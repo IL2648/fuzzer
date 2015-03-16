69e3b7d7b4cd531820748cbd565d023e385a8118
Zachary Nielsen <zen.gameman@gmail.com>
2015-03-16 12:03:03 -0400

    fixed unpacking, added slow and status codes

--- sanitization.py
+++ sanitization.py
@@ -1,8 +1,12 @@
 import bleach
 import requests
+import time
 urlInputDict = {"https://www.google.com/webhp":["sourceid","ion","espv","ie"]}
 formInputDict = {"https://www.google.com/webhp":["sourceid","ion","espv","ie"]}
 unsanitized = []
+statusCodeLog = []
+slowLog = []
+slowTime = 500
 def sanitization(s):
 	checkForms(s)
 	checkURLs(s)
@@ -10,18 +14,28 @@ def sanitization(s):
 def checkForms(s):
 	for k,v in formInputDict:
 		url = k + "?"
-		for e in v:
-			url = url + v + "=<>\\\"&"
+		for e in formInputDict[v]:
+			url = url + e + "=<>\\\"&"
+		start = = lambda: int(round(time.time() * 1000))
 		r = s.post(url)
+		finish = = lambda: int(round(time.time() * 1000))
+		if(finish - start > slowTime):
+			slowLog.append(url)
 		isSanitized(r)
+		checkResponse(r)
 
 def checkURLs(s):
-	for k,v in urlInputDict:
+	for k in urlInputDict:
 		url = k + "?"
-		for e in v:
-			url = url + v + "=<>\\\"&"
+		for e in urlInputDict[v]:
+			url = url + e + "=<>\\\"&"
+		start = = lambda: int(round(time.time() * 1000))
 		r = s.post(url)
+		finish = = lambda: int(round(time.time() * 1000))
+		if(finish - start > slowTime):
+			slowLog.append(url)
 		isSanitized(r)
+		checkResponse(r)
 
 def isSanitized(r):
 	if(r.url == bleach.clean(r.url)):
@@ -31,5 +45,10 @@ def isSanitized(r):
 		print r.url + "!=" + bleach.clean(r.url)
 		unsanitized.append(r.url)
 		return
+
+def checkResponse(r):
+    if( r.status_code >= 300 ):
+        statusCodeLog.append((r.status_code,newURL))
+
 with requests.Session() as s:
 	sanitization(s)
